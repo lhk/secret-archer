e7477a51c9fdc35632b50ef6dad75cfac9b12198
Max Beikirch <max.beikirch@unitybox.de>
2013-05-14 16:12:49 +0200

    Support for dependency installation by any makefile generator.

--- cmake/installSFML.cmake
+++ cmake/installSFML.cmake
@@ -8,17 +8,17 @@ set(SFMLDIR ${TMPDIR}/SFML)
 delete(${SFMLDIR})
 git_clone(SFML git://github.com/LaurentGomila/SFML.git ${TMPDIR})
 git_checkout(2.0 ${SFMLDIR})
-cmake_configure(${SFMLDIR} -G${SFML_MAKEFILE_GENERATOR} -DCMAKE_INSTALL_PREFIX=${SFML_INSTALL_PREFIX})
-make_install("Building and installing SFML release libraries" ${SFMLDIR})
+cmake_configure(${SFMLDIR} -G${SFML_MAKEFILE_GENERATOR} -DCMAKE_MAKE_PROGRAM=${MAKE_PROGRAM} -DCMAKE_INSTALL_PREFIX=${SFML_INSTALL_PREFIX})
+make_install(${MAKE_PROGRAM} "Building and installing SFML release libraries" ${SFMLDIR})
 
 IF(${BUILD_SFML_DEBUG_LIBS})
   cmake_configure(${SFMLDIR} -DCMAKE_BUILD_TYPE=Debug)
-  make_install("Building and installing SFML debug libraries" ${SFMLDIR})
+  make_install(${MAKE_PROGRAM} "Building and installing SFML debug libraries" ${SFMLDIR})
 ENDIF()
 
 IF(${BUILD_SFML_DOC})
   # ERROR_QUIET to suppress doxygen messages.
   cmake_configure(${SFMLDIR} -DSFML_BUILD_DOC=TRUE)
-  make_install("Building and installing SFML docs" ${SFMLDIR} ERROR_QUIET)
+  make_install(${MAKE_PROGRAM} "Building and installing SFML docs" ${SFMLDIR} ERROR_QUIET)
 ENDIF()
 
--- cmake/installThor.cmake
+++ cmake/installThor.cmake
@@ -8,14 +8,14 @@ delete(${THORDIR})
 git_clone(Thor git://github.com/Bromeon/Thor.git ${TMPDIR})
 
 cmake_configure(${THORDIR} -G${SFML_MAKEFILE_GENERATOR} -DSFML_INCLUDE_DIR=${SFML_INSTALL_PREFIX}/include -DCMAKE_INSTALL_PREFIX=${THOR_INSTALL_PREFIX} -DSFML_ROOT=${SFML_INSTALL_PREFIX})
-make_install("Building and installing Thor release library" ${THORDIR})
+make_install(${MAKE_PROGRAM} "Building and installing Thor release library" ${THORDIR})
 
 if(${BUILD_SFML_DEBUG_LIBS})
   cmake_configure(${THORDIR} -DCMAKE_BUILD_TYPE=Debug)
-  make_install("Building and installing Thor debug library" ${THORDIR})
+  make_install(${MAKE_PROGRAM} "Building and installing Thor debug library" ${THORDIR})
 endif()
 
 if(${BUILD_SFML_DOC})
   cmake_configure(${THORDIR} -DTHOR_BUILD_DOC=TRUE)
-  make_install("Building and installing Thor docs" ${THORDIR} ERROR_QUIET)
+  make_install(${MAKE_PROGRAM} "Building and installing Thor docs" ${THORDIR} ERROR_QUIET)
 endif()
